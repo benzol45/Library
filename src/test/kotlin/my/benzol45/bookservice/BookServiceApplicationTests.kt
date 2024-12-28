1e0d0cb1df3a6df7f7fca6f0125a3c63c73758fc
mikhail kalinin <benzol45@gmail.com>
2024-12-28 09:56:24 +0100

    Add project skeleton

---
+++ src/test/kotlin/my/benzol45/bookservice/BookServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.benzol45.bookservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
